9c70e03a0f0a4f89185be191e20568b570ad8ab3
Perkles <jenniferramosdecarvalho@gmail.com>
2022-07-14 10:10:35 -0300

    sobrecarga quadrilátero

---
+++ src/sobrecarga/java/Main.java
@@ -0,0 +1,11 @@
+package sobrecarga.java;
+
+public class Main {
+    public static void main(String [] args) {
+
+        System.out.println("Exercício Quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5,5);
+        Quadrilatero.area3(  7 ,8,  9 );
+    }
+}
